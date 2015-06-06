a6686f7ab5a5bc6dc3de0c0f970edb8a1d01df2e
Friedrich Lindenberg <friedrich@pudo.org>
2015-06-06 22:57:02 +0200

    Begin implementing results presentation in the table.

--- src/cubes.js
+++ src/cubes.js
@@ -64,19 +64,69 @@ ngCubes.directive('cubesTable', ['$rootScope', function($rootScope) {
     },
     templateUrl: 'angular-cubes-templates/table.html',
     link: function(scope, element, attrs, cubesCtrl) {
-      scope.model = {};
-      scope.query = {};
-      scope.data = {};
+      var model = null, query = {};
+      scope.columns = [];
+      scope.headers = [];
+      scope.table = [];
 
-      $rootScope.$on(cubesCtrl.modelUpdate, function(event, model) {
-        scope.model = model;
-        console.log(model);
+      $rootScope.$on(cubesCtrl.modelUpdate, function(event, m) {
+        model = m;
       });
 
-      $rootScope.$on(cubesCtrl.dataUpdate, function(event, data, query) {
-        scope.query = query;
-        scope.data = data;
-        console.log(query, data);
+      $rootScope.$on(cubesCtrl.dataUpdate, function(event, data, q) {
+        query = q;
+        if (!model) {
+          return;
+        }
+
+        var headers = [], columns = [], dimensions = [];
+        for (var d in data.levels) {
+          for (var j in model.dimensions) {
+            var dim = model.dimensions[j];
+            if (dim.name == d) {
+              dimensions.push(dim);
+            }
+          }
+        }
+        for (var i in dimensions) {
+          var d = dimensions[i], dprefix = d.name + '.',
+              span = 0;
+          for (var j in data.attributes) {
+            var a = data.attributes[j];
+            if (a.startsWith(dprefix)) {
+              var name = a.split('.')[1];
+              columns.push({
+                name: name,
+                column: a,
+                type: 'text'
+              });
+              span++;
+            }
+          }
+          headers.push({
+            label: d.label,
+            colspan: span
+          })
+        }
+
+        // aggregates metadata
+        for (var i in data.aggregates) {
+          var a = data.aggregates[i];
+          for (var j in model.aggregates) {
+            var attr = model.aggregates[j];
+            if (attr.name == a) {
+              attr.type = 'numeric';
+              attr.column = attr.name;
+              attr.colspan = 1;
+              columns.push(attr);
+              headers.push(attr);
+            }
+          }
+        }
+
+        scope.table = data.cells;
+        scope.columns = columns;
+        scope.headers = headers;
       });
 
       cubesCtrl.init();
