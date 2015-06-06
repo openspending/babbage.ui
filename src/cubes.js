e8fe968190b19e7c81a0b97a0ab51f7097077686
Friedrich Lindenberg <friedrich@pudo.org>
2015-06-06 16:35:35 +0200

    Work out communication between nested directives.

--- src/cubes.js
+++ src/cubes.js
@@ -1,28 +1,53 @@
 
 var ngCubes = angular.module('ngCubes', ['ngCubes.templates']);
 
-ngCubes.directive('cubes', ['$http', function($http) {
+var makeSignal = function(name) {
+  return 'cubes_' + Math.random().toString(36).replace(/[^a-z]+/g, '');
+};
+
+ngCubes.directive('cubes', ['$http', '$rootScope', function($http, $rootScope) {
   return {
     restrict: 'E',
     transclude: true,
     scope: {
-      slicer: '=',
-      cube: '='
+      slicer: '@',
+      cube: '@',
+      state: '='
     },
     templateUrl: 'angular-cubes-templates/cubes.html',
     controller: function($scope) {
-      $scope.query = {'huhu': 'haha'};
+      var self = this,
+          state = $scope.state || {},
+          api = $scope.slicer.slice(),
+          api = api.endsWith('/') ? api.slice(0, api.length - 1) : api,
+          api = api + '/cube/' + $scope.cube;
 
-      console.log($scope.slicer);
+      self.dataUpdate = makeSignal();
+      self.modelUpdate = makeSignal();
 
-      this.getQuery = function() {
-        return $scope.query;
+      self.reset = function() {
+        $http.get(api + '/model').then(function(res) {
+          $rootScope.$broadcast(self.modelUpdate, res.data);
+        });
+        self.update({});
+      };
+
+      self.getQuery = function() {
+        return state.query;
+      };
+
+      self.update = function(newQuery) {
+        state.query = newQuery;
+        var reqQuery = angular.copy(newQuery);
+        $http.get(api + '/aggregate').then(function(res) {
+          $rootScope.$broadcast(self.dataUpdate, res.data, reqQuery);
+        });
       };
     }
   };
 }]);
 
-ngCubes.directive('cubesTable', ['$http', function($http) {
+ngCubes.directive('cubesTable', ['$rootScope', function($rootScope) {
   return {
     restrict: 'EA',
     require: '^cubes',
@@ -31,7 +56,21 @@ ngCubes.directive('cubesTable', ['$http', function($http) {
     },
     templateUrl: 'angular-cubes-templates/cubes-table.html',
     link: function(scope, element, attrs, cubesCtrl) {
-      console.log(cubesCtrl.getQuery());
+      scope.model = {};
+      scope.query = {};
+      scope.data = {};
+
+      $rootScope.$on(cubesCtrl.modelUpdate, function(event, model) {
+        scope.model = model;
+        console.log(model);
+      });
+
+      $rootScope.$on(cubesCtrl.dataUpdate, function(event, data, query) {
+        scope.query = query;
+        scope.data = data;
+      });
+
+      cubesCtrl.reset();
     }
   };
 }]);
