f0c8d1d5177ed168d28a65cabf9bd7b37f322a7a
Igor Borysyuk <borysyuk@qarea.com>
2016-03-28 17:16:21 +0300

    old code has been removed

--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
-//require('./app')
-//require('./api')
-//require('./babbage')
-//require('./chart')
-//require('./crosstab')
-//require('./facts')
-//require('./pager')
-//require('./panel')
-//require('./sankey')
-//require('./treemap')
-//require('./util')
-//require('./workspace')
-//
-//module.exports = 'ngBabbage'
-
 import Api from './api/index'
 import * as Utils from './components/utils.js'
 import * as BabbageClasses from './bindings/angular/index'
