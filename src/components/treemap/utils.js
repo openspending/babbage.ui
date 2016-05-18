47e84b6214caefa2a15b358402486f5438cc6d6d
Maik Lukasch <mlukasch@gmail.com>
2016-05-18 18:22:48 +0200

    fixing text overflow checking for treemap boxes

--- src/components/treemap/utils.js
+++ src/components/treemap/utils.js
@@ -3,10 +3,9 @@
  */
 module.exports.checkForTextOverflow = function(selector, textOverflowHandler){
     var result = false;
-    var rectangles = $(selector);
-    rectangles.each(function(index, item){
+    var elementsForTextOverflowCheck = $(selector);
+    elementsForTextOverflowCheck.each(function(index, item){
         if(($(item)[0].offsetWidth < $(item)[0].scrollWidth) || ($(item)[0].offsetHeight < $(item)[0].scrollHeight)){
-            $(item).empty();
             textOverflowHandler(item);
             result = true;
         }
