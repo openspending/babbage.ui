c2ba47888a9d4a33b35331e6b74b6caf4eb5c092
Levko Kravets <levko.ne@gmail.com>
2016-11-28 15:35:03 +0200

    Improve build scripts and remove some garbage

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,18 +1,21 @@
-'use strict'
+'use strict';
 
-var buildTarget = process.env.BUILD_TARGET || '';
-
-var targetPath = './src/index.js';
-if (buildTarget) {
-  var ext = '.js';
-  if (buildTarget == 'vuejs') {
-    ext = '.vue';
-  }
-  targetPath = './src/bindings/' + buildTarget + '/index' + ext;
-}
+var fs = require('fs');
+var _ = require('lodash');
 
 module.exports = {
-  entry:  targetPath,
+  entry:  _.chain(fs.readdirSync('./src/bindings'))
+    .map(function(item) {
+      var ext = item == 'vuejs' ? '.vue' : '.js';
+      return [item, './src/bindings/' + item + '/index' + ext];
+    })
+    .fromPairs()
+    .value(),
+  output: {
+    path: './dist',
+    library: 'Babbage',
+    libraryTarget: 'umd'
+  },
   devtool: 'source-map',
   externals: {
     // require("jquery") is external and available
@@ -28,6 +31,5 @@ module.exports = {
       { test: /\.js$/, loaders: [ 'babel-loader' ], exclude: /node_modules/ },
       { test: /\.html$/, loader: 'raw' }
     ]
-  },
-  output: { library: 'Babbage', libraryTarget: 'umd' }
-}
+  }
+};
