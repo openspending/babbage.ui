61eeaba4caa1f1be9ac0cb8b15a8ac0090031671
Vitor Baptista <vitor@vitorbaptista.com>
2017-05-16 12:42:20 +0100

    [#1197] Add "maxSlices" pie chart param to group slices into "Others"
    A pie chart doesn't work well if there're many categories, so this code adds a
    "maxSlices" parameter to it where, if there're more slices than this number,
    it'll group them in a single "Others" slice.
    
    We do this by getting the totals from the "summary" attribute, and subtracting
    the values of all slices that will still be shown.
    
    This works OK but there's a caveat: it'll break if we're using anything other
    tham a simple sum. For example, if the data contains averages we'll display
    incorrect data.
    
    In my opinion, this is OK, as a pie chart isn't well suited to display averages
    anyway. We just need to take care not to use averages (or other summary methods)
    when limiting the number of slices.
    
    opentrials/opentrials#1197

--- src/components/pie/index.js
+++ src/components/pie/index.js
@@ -7,7 +7,7 @@ import events from 'events'
 
 export class PieChartComponent extends events.EventEmitter {
 
-  constructor() {
+  constructor(i18n) {
     super();
 
     this._api = null;
@@ -17,6 +17,12 @@ export class PieChartComponent extends events.EventEmitter {
     this.downloader = null;
 
     this.formatValue = null;
+
+    this.i18n = i18n;
+
+    if (this.i18n === undefined) {
+      this.i18n = (val) => val;
+    }
 
     // Prevent from throwing exception in EventEmitter
     this.on('error', (sender, error) => {
@@ -42,20 +48,18 @@ export class PieChartComponent extends events.EventEmitter {
     };
   }
 
-  build(endpoint, cube, params, wrapper, colorSchema) {
+  build(endpoint, cube, params, wrapper, maxSlices=5, colorSchema) {
     params = _.cloneDeep(params);
 
     var that = this;
     this.wrapper = wrapper;
 
-    that.emit('loading', that);
-
-    var api = this.getApiInstance();
-    api.aggregate(endpoint, cube, params)
+    return this._getData(endpoint, cube, params, maxSlices)
       .then((data) => {
         var columns = Utils.buildC3PieColumns(data, params.aggregates);
         var colors = {};
         _.each(columns, (value, index) => {
+          // FIXME: colorSchema should be used here
           colors[value[0]] = Utils.colorScale(index);
         });
 
@@ -90,13 +94,82 @@ export class PieChartComponent extends events.EventEmitter {
           }
         });
 
-        that.emit('loaded', that, data);
         that.emit('ready', that, data, null);
       })
       .catch((error) => {
         that.emit('error', that, error);
         that.emit('ready', that, null, error);
       });
+  }
+
+  _getData(endpoint, cube, params, maxSlices) {
+    var that = this;
+    that.emit('loading', that);
+
+    var api = this.getApiInstance();
+    return api.aggregate(endpoint, cube, params)
+      .then((data) => that._groupSlicesIfMoreThan(data, maxSlices))
+      .then((data) => {
+        that.emit('loaded', that, data);
+        return data;
+      })
+      .catch((error) => {
+        that.emit('error', that, error);
+        that.emit('ready', that, null, error);
+      });
+  }
+
+  _groupSlicesIfMoreThan(data, maxSlices) {
+    if (maxSlices <= 0) {
+      throw RangeError(`Can't create a pie chart with less than 1 slice (asked for ${maxSlices} slices)`);
+    }
+    if (!_.isArray(data.cells) || maxSlices === undefined || data.cells.length <= maxSlices) {
+      return data;
+    }
+
+    const that = this;
+
+    const individualCells = data.cells.slice(0, maxSlices - 1);
+    const othersCell = {
+      dimensions: data.cells[0].dimensions.map((dimension) => {
+        return Object.assign(
+          {},
+          dimension,
+          {
+            keyValue: 'others',
+            nameValue: that.i18n('others'),
+          }
+        );
+      }),
+
+      // Set measures as the total values minus the individual cell's values
+      measures: data.cells[0].measures.map((measure) => {
+        const individualCellsTotal = individualCells.reduce((total, cell) => {
+          const cellMeasure = cell.measures.find((m) => m.key === measure.key);
+          return total + cellMeasure.value;
+        }, 0);
+
+        return Object.assign(
+          {},
+          measure,
+          {
+            value: data.summary[measure.key] - individualCellsTotal,
+          }
+        );
+      }),
+    };
+
+
+    return Object.assign(
+      {},
+      data,
+      {
+        cells: [
+          ...individualCells,
+          othersCell,
+        ]
+      }
+    );
   }
 }
 
---
+++ test/component-pie.js
@@ -0,0 +1,181 @@
+const nock = require('nock');
+const assert = require('chai').assert;
+const _ = require('lodash');
+
+describe('Babbage.ui pie component', function() {
+  const cleanupJSDOM = require('jsdom-global')();
+  const aggregate2 = require('./data/component-pie/aggregate2.json');
+  const test2PackageModel = require('./data/component-pie/package2Model.json');
+  var pieComponent;
+
+  before(() => {
+    // FIXME: We need to define these because of https://github.com/progers/pathseg/issues/19
+    window.SVGPathSeg = function() {};
+    window.SVGPathSegList = function() {};
+
+    pieComponent = new (require('../src/components/pie').PieChartComponent)();
+
+    nock('http://example.com/')
+      .persist()
+      .get('/cubes/test2/aggregate')
+      .query({
+        drilldown: 'administrative_classification_admin3_code.admin3_code' +
+          '|administrative_classification_admin3_code.admin3_label',
+        pagesize: '30',
+        order: 'approved.sum:desc',
+      })
+      .reply(200, aggregate2, {'access-control-allow-origin': '*'});
+
+    nock('http://example.com/')
+      .persist()
+      .get('/cubes/test2/model')
+      .reply(200, test2PackageModel, {'access-control-allow-origin': '*'});
+  });
+
+  after(() => {
+    cleanupJSDOM();
+  });
+
+  it('Should exists', () => {
+    assert(_.isObject(pieComponent));
+  });
+
+  it('Should return some data', () => {
+    const params = {
+      aggregates: 'approved.sum',
+      group: ['administrative_classification_admin3_code.admin3_code']
+    };
+
+    return pieComponent._getData('http://example.com', 'test2', params)
+      .then(function(data) {
+        assert.isDefined(data);
+        assert.lengthOf(data.cells, 3);
+      });
+  });
+
+  describe('Grouping data', () => {
+    it('Should return the unmodified data if it has no cells', () => {
+      const data = {
+        cells: [],
+      };
+      assert.deepEqual(pieComponent._groupSlicesIfMoreThan(data), data);
+    });
+
+    it('Should return the unmodified data if it has less cells than maxSlices', () => {
+      const data = {
+        cells: [{}],
+      };
+      assert.deepEqual(pieComponent._groupSlicesIfMoreThan(data, 1), data);
+    });
+
+    it('Should return the unmodified data if maxSlices is undefined', () => {
+      const data = {
+        cells: [{}],
+      };
+      assert.deepEqual(pieComponent._groupSlicesIfMoreThan(data), data);
+    });
+
+    it('Should throw RangeError if maxSlices is less than 1', () => {
+      const data = {};
+
+      for (const maxSlices of [-1, 0]) {
+        assert.throws(
+          () => pieComponent._groupSlicesIfMoreThan(data, maxSlices),
+          RangeError
+        );
+      }
+    });
+
+    it('Should set the "others" cell\'s dimension values to i18n("others")', () => {
+      const data = _stubAggregateData([
+        100,
+        200,
+      ]);
+      const maxSlices = 1;
+
+      const grouppedData = pieComponent._groupSlicesIfMoreThan(_.cloneDeep(data), maxSlices);
+      const othersCell = grouppedData.cells[grouppedData.cells.length - 1];
+      othersCell.dimensions.forEach((dimension) => {
+        Object.keys(dimension).forEach((dimensionKey) => {
+          if (dimensionKey.endsWith('Value')) {
+            assert.equal(dimension[dimensionKey], pieComponent.i18n('others'));
+          } else {
+            assert.notEqual(dimension[dimensionKey], pieComponent.i18n('others'));
+          }
+        });
+      });
+    });
+
+    it('Should work with a single slice', () => {
+      const data = _stubAggregateData([
+        100,
+        200,
+        300,
+        500,
+      ]);
+      const maxSlices = 1;
+
+      const grouppedData = pieComponent._groupSlicesIfMoreThan(_.cloneDeep(data), maxSlices);
+      const othersCell = grouppedData.cells[grouppedData.cells.length - 1];
+      assert.lengthOf(grouppedData.cells, maxSlices);
+      othersCell.measures.forEach((measure) => {
+        // If there's only one slice, its values must be equal to the summary values
+        assert.equal(measure.value, data.summary[measure.key]);
+      });
+    });
+
+    it('Should work with more than one slice', () => {
+      const data = _stubAggregateData([
+        100,
+        200,
+        300,
+        500,
+      ]);
+      const maxSlices = 2;
+
+      const grouppedData = pieComponent._groupSlicesIfMoreThan(_.cloneDeep(data), maxSlices);
+      const othersCell = grouppedData.cells[grouppedData.cells.length - 1];
+      assert.lengthOf(grouppedData.cells, maxSlices);
+      othersCell.measures.forEach((measure) => {
+        assert.equal(measure.value, 1000);
+      });
+    });
+  });
+});
+
+
+function _stubAggregateData(cellsMeasuresValues) {
+  const MEASURE = {
+    key: 'approved.sum',
+    name: 'approved',
+  };
+  const data = {};
+
+  data.cells = cellsMeasuresValues.map((value) => {
+    return {
+      dimensions: [{
+        keyField: 'KEY_FIELD',
+        keyValue: 'KEY_VALUE',
+        nameField: 'NAME_FIELD',
+        nameValue: 'NAME_VALUE',
+      }],
+      measures: [
+        {
+          key: MEASURE.key,
+          name: MEASURE.name,
+          value,
+        },
+      ],
+    };
+  });
+
+  data.summary = data.cells.reduce((summary, cell) => {
+    const measure = cell.measures.find((measure) => measure.key === MEASURE.key);
+    summary[MEASURE.key] += measure.value;
+    return summary;
+  }, {
+    [MEASURE.key]: 0,
+  });
+
+  return data;
+}
